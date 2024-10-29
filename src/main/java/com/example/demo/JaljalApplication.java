ba2167d426633de87378811b25daf52fd92131c8
Keon <devkeon123@gmail.com>
2024-10-29 19:36:36 +0900

    init

---
+++ src/main/java/com/example/demo/JaljalApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaljalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaljalApplication.class, args);
+	}
+
+}
